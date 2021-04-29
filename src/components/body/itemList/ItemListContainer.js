e18c119f0b76a66f6122d2f7932dbb00c39890f8
Cufavarela <cufa.varela@gmail.com>
2021-04-29 18:22:16 -0300

    aocmodar componentes

--- src/components/body/itemList/ItemListContainer.js
+++ src/components/body/itemList/ItemListContainer.js
@@ -1,13 +1,27 @@
-import { ItemList } from "./ItemList";
-import "./itemList.scss";
+import { useEffect, useState } from "react";
+import productsMock from "../mock/productsMock";
+import ItemList from "./ItemList";
+import Loader from "../../loader/loader";
 
-const ItemListContainer = ({ greeting }) => {
-  return (
-    <>
-      <h1>{greeting} 🛠</h1>
-      <ItemList />
-    </>
-  );
+const ItemListContainer = () => {
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const getProducts = new Promise((res, rej) => {
+    setTimeout(function () {
+      res(productsMock);
+    }, 2000);
+  });
+
+  useEffect(() => {
+    setIsLoading(true);
+    getProducts
+      .then((res) => setProducts(res))
+      .catch((err) => alert(err))
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  return <>{isLoading ? <Loader /> : <ItemList products={products} />}</>;
 };
 
 export default ItemListContainer;
