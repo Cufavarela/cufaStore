0148eca2c5c89bf4761a453b91242e191e303583
Cufavarela <cufa.varela@gmail.com>
2021-04-29 18:26:16 -0300

    acomodar componentes

--- src/components/body/itemList/ItemListContainer.js
+++ src/components/body/itemList/ItemListContainer.js
@@ -1,12 +1,28 @@
+import { useEffect, useState } from "react";
 import { ItemList } from "./ItemList";
+import { productsMock } from "../mock/productsMock";
+import Loader from "../../loader/loader";
 import "./itemList.scss";
 
 const ItemListContainer = () => {
-  return (
-    <>
-      <ItemList />
-    </>
-  );
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const getProducts = new Promise((res, rej) => {
+    setTimeout(function () {
+      res(productsMock);
+    }, 2000);
+  });
+
+  useEffect(() => {
+    setIsLoading(true);
+    getProducts
+      .then((res) => setProducts(res))
+      .catch((err) => alert(err))
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  return <>{isLoading ? <Loader /> : <ItemList products={products} />}</>;
 };
 
 export default ItemListContainer;
