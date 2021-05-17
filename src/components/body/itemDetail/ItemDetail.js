37090484c9cf1965a9725aaac1394bf27422349e
Cufavarela <cufa.varela@gmail.com>
2021-05-17 20:37:53 -0300

    add itemDetail

--- src/components/body/itemDetail/ItemDetail.js
+++ src/components/body/itemDetail/ItemDetail.js
@@ -24,7 +24,7 @@ export const ItemDetail = ({ product }) => {
 
   return (
     <div className="itemDetail">
-      <img src={product.img} alt="product" />
+      <img src="http://placekitten.com/g/200/300" alt="product" />
       <h2>{product.name}</h2>
       <h4>${product.price}</h4>
       <ItemsCount stock={product.stock} initial={1} onAdd={onAdd} />
--- src/components/body/itemDetail/ItemDetailContainer.js
+++ src/components/body/itemDetail/ItemDetailContainer.js
@@ -2,29 +2,26 @@ import { useEffect, useState } from "react";
 import "./itemDetail.scss";
 import Loader from "../../loader/loader";
 import { ItemDetail } from "./ItemDetail";
-import { productsMock } from "../mock/productsMock";
 import { useParams } from "react-router";
+import { getFirestore } from "../../../firebase/firebaseConfig";
 
 export const ItemDetailContainer = () => {
   const { id } = useParams();
   const [product, setProduct] = useState({});
   const [isLoading, setIsLoading] = useState(false);
 
-  const getProducts = new Promise((res, rej) => {
-    setTimeout(function () {
-      res(productsMock);
-    }, 2000);
-  });
-
   useEffect(() => {
     setIsLoading(true);
-    getProducts
-      .then((res) => {
-        res.forEach((item) => {
-          if (item.id === id) {
-            setProduct(item);
-          }
-        });
+    const db = getFirestore();
+    const itemsCollection = db.collection("items");
+    const item = itemsCollection.doc(id);
+    item
+      .get()
+      .then((doc) => {
+        if (!doc.exists) {
+          console.log("No existe");
+        }
+        setProduct({ id: doc.id, ...doc.data() });
       })
       .catch((err) => alert(err))
       .finally(() => setIsLoading(false));
